8e9cadc5e67469eb13bafc78c0e21e26345af420
kijko <ae86trueno>
2018-11-03 20:47:45 +0100

    initial2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'predict-match-result-client';
+  teams: TeamData[] = [];
+
+  constructor(private http: HttpClient) {
+  }
+
+  loadTeams() {
+    this.http
+      .get<TeamData[]>('http://localhost:8080/api/teams/availibleToPredict')
+      .subscribe(teams => {
+        this.teams = teams;
+      });
+  }
+
+  deleteTeams() {
+    this.teams = [];
+  }
+}
+
+interface TeamData {
+  id: number;
+  name: string;
+  linkToImage: string;
 }
